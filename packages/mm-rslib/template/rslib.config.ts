bcf4523f67170d70f8b5a04fbc11751dd3c3be2f
elcoosp <elcoosp@gmail.com>
2024-11-28 01:34:52 +0100

    fix: add missing scripts & fix biome config + add dummy test to pass ci

--- packages/mm-rslib/template/rslib.config.ts
+++ packages/mm-rslib/template/rslib.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@rslib/core'
 import { createRsLibConfig } from '@elcoosp-configs/rslib'
+import { defineConfig } from '@rslib/core'
 
 export default defineConfig(async () => createRsLibConfig({ preset: 'dual' }))
