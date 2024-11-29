311e293c61f066a0cdbdd5fd98765d28eb37de96
elcoosp <elcoosp@gmail.com>
2024-11-29 10:50:57 +0100

    ci: fix tests use one global vite config ignoring template folders

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   // Configure Vitest (https://vitest.dev/config/)
-  test: {},
+  test: {
+
+    exclude:[
+      "**/template/**"
+    ]
+  },
 })
