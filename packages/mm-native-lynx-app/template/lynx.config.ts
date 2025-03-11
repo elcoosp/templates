e3fd5519cb1f891e07b73381f16f954f7b6e0b9c
elcoosp <elcoosp@gmail.com>
2025-03-11 23:19:50 +0100

    fix: hardcode lynxpo:copy-native-asset plugin to copy bundle to native app asset folder, fix kotlinCompilerExtensionVersion, use alias for assets moved one folder up & fix tsconfig

--- packages/mm-native-lynx-app/template/lynx.config.ts
+++ packages/mm-native-lynx-app/template/lynx.config.ts
@@ -1,16 +1,46 @@
-import { defineConfig } from '@lynx-js/rspeedy'
+import { defineConfig } from "@lynx-js/rspeedy";
+import { pluginReactLynx } from "@lynx-js/react-rsbuild-plugin";
+import fs from "fs/promises";
 
-import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin'
-import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
+const copyNativeAssets = async (
+  platform: "android" | "ios",
+  projectName: string,
+) => {
+  if (platform === "ios") throw new Error(`Unsupported platform: ${platform}`);
+  const langs = { ios: ["Swift", "Objc"], android: ["Kotlin", "Java"] }[
+    platform
+  ];
 
+  const fromFolder = `dist`;
+  const toFolder = `${platform}/${projectName}/app/src/main/assets`;
+  await fs.cp(fromFolder, toFolder, { recursive: true });
+};
+
+function pluginCopyNativeAssets(
+  platform: "android" | "ios",
+  projectName: string,
+) {
+  return {
+    name: "lynxpo:copy-native-asset",
+    setup(api) {
+      api.onAfterBuild(
+        async () => await copyNativeAssets(platform, projectName),
+      );
+    },
+  };
+}
 export default defineConfig({
   plugins: [
-    pluginQRCode({
-      schema(url) {
-        // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
-        return `${url}?fullscreen=true`
-      },
-    }),
+    pluginCopyNativeAssets("android", "Kotlin{{project_name}}"),
     pluginReactLynx(),
   ],
-})
+  environments: {
+    web: {},
+    lynx: {},
+  },
+  source: {
+    alias: {
+      "@assets": "./assets",
+    },
+  },
+});
