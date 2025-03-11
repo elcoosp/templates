6aa63e4906bfa657c1f568fac54d09f9f550708c
elcoosp <elcoosp@gmail.com>
2025-03-11 21:05:19 +0100

    feat(mm-native-lynx-app): add typescript to template

--- packages/mm-native-lynx-app/template/rslib.config.ts
+++
@@ -1,4 +0,0 @@
-import { createRsLibConfig } from '@elcoosp-configs/rslib'
-import { defineConfig } from '@rslib/core'
-
-export default defineConfig(async () => createRsLibConfig({ preset: 'dual' }))
