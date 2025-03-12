1332b7b000c4c8eddca7ffbb09ef9f521e299818
elcoosp <elcoosp@gmail.com>
2025-03-12 13:28:09 +0100

    fix(lynx): lint issues targetSdk 35

--- packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/build.gradle.kts
+++ packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.{{org}}.{{project_name|camel_case}}"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
