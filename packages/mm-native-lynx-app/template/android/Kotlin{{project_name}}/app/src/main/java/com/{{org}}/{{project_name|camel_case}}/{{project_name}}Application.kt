732eabccce841d151374ed6d990577126a7b5def
elcoosp <elcoosp@gmail.com>
2025-03-19 23:21:22 +0100

    feat(lynx): input

--- packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/src/main/java/com/{{org}}/{{project_name|camel_case}}/{{project_name}}Application.kt
+++ packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/src/main/java/com/{{org}}/{{project_name|camel_case}}/{{project_name}}Application.kt
@@ -1,9 +1,12 @@
 package com.{{org}}.{{project_name|camel_case}}
 
+import android.view.View
 import lynxpo.core.LynxpoApp
+import lynxpo.core.LynxpoUI
 import lynxpo.core.modules.clipboard.ClipboardModule
 import lynxpo.core.modules.device.DeviceModule
 import lynxpo.core.modules.statusbar.StatusBarModule
+import lynxpo.core.modules.ui.input.InputUI
 
 class MyProjApplication : LynxpoApp() {
     override val lynxpoModules
@@ -11,6 +14,9 @@ class MyProjApplication : LynxpoApp() {
             Pair("ClipboardModule", ClipboardModule::class.java),
             Pair("DeviceModule", DeviceModule::class.java),
             Pair("StatusBarModule", StatusBarModule::class.java)
-            
+        )
+    override val lynxpoUiModules: Array<Pair<String, Class<out LynxpoUI<out View>>>>
+        get() = arrayOf(
+            Pair("input", InputUI::class.java)
         )
 }
--- packages/mm-native-lynx-app/template/src/routes/App.tsx
+++ packages/mm-native-lynx-app/template/src/routes/App.tsx
@@ -15,6 +15,7 @@ export function App() {
     'background only'
     setAlterLogo(!alterLogo)
     setBrand(NativeModules.DeviceModule.brand())
+    NativeModules.StatusBarModule.setColor()
   }, [alterLogo])
 
   const nav = useNavigate()
@@ -33,12 +34,16 @@ export function App() {
           <text className="Title">React</text>
           <text className="Subtitle">on Lynx</text>
           <text className="Subtitle">with phone brand from native {brand}</text>
+          <Input />
         </view>
         <view className="Content">
           <image src={arrow} className="Arrow" />
           <text className="Description">Tap the logo and have fun!</text>
           <text className="Hint">
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/routes/App.tsx '}</text>
+            Edit
+            <text style={{ fontStyle: 'italic' }}>
+              {' src/routes/App.tsx '}
+            </text>
             to see updates!
           </text>
           <text className="text-3xl font-bold">Hello world from tailwind!</text>
@@ -50,6 +55,49 @@ export function App() {
         </view>
         <view style={{ flex: 1 }}></view>
       </view>
+    </view>
+  )
+}
+export function Input() {
+  const [inputValue, setInputValue] = useState('')
+  const [focusInputError, setFocusInputError] = useState('')
+
+  const handleInput = (e) => {
+    const currentValue = e.detail.value.trim()
+    setInputValue(currentValue)
+  }
+
+  const requestFocus = () => {
+    lynx
+      .createSelectorQuery()
+      .select('#input-id')
+      .invoke({
+        method: 'focus',
+        params: {},
+        success: function (res) {
+          console.log('lynx', 'request focus success')
+        },
+        fail: function (res) {
+          setFocusInputError(JSON.stringify(res))
+          console.log('lynx', 'request focus fail')
+        },
+      })
+      .exec()
+  }
+
+  return (
+    <view className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex-1">
+      <text className="block text-gray-700 text-sm font-bold mb-2">
+        Card URL {focusInputError}
+      </text>
+      <input
+        id="input-id"
+        className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        bindinput={handleInput}
+        bindtap={requestFocus}
+        value={inputValue}
+        placeholder="Enter Card URL"
+      />
     </view>
   )
 }
