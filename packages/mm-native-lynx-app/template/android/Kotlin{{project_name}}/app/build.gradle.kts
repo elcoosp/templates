6382a6133559e9edafec0cd99bc5c73e83c3649f
elcoosp <elcoosp@gmail.com>
2025-03-20 13:01:28 +0100

    chore(lynx): rm java template

--- packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/build.gradle.kts
+++ packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/build.gradle.kts
@@ -26,7 +26,8 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
+                getDefaultProguardFile("proguard-android.txt")
             )
         }
     }
