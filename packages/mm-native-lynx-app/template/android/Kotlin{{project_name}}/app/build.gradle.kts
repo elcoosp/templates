6b2c7f9a8c2e18b523c9293a33b7f0a766ea6525
elcoosp <elcoosp@gmail.com>
2025-03-22 17:54:46 +0100

    feat(nmb/ktts/core): use jdk 21

--- packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/build.gradle.kts
+++ packages/mm-native-lynx-app/template/android/Kotlin{{project_name}}/app/build.gradle.kts
@@ -31,10 +31,10 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions { jvmTarget = "21" }
     buildFeatures { compose = true }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
